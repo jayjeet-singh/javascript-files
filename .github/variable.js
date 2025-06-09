18aa24819a59db510810a05c2b0dd54c5406ad67
jayjeet-singh <rajputjayjeetsingh20@gmail.com>
2025-06-09 08:38:58 +0000

    new changes

---
+++ .github/variable.js
@@ -0,0 +1,15 @@
+const MyId = 12345
+let MyPassword = "67890"
+var MyEmail = "aaaa@gmail"
+MyCity = "Bhopal"
+
+let MyAccount 
+
+//MyID = 88888 cannot be changed always remain constant
+
+//MyPassword = "99999"   can be changed
+//MyEmail = "jjjj@gmail"  can be changed but prefer not to use because of issue in  block scope & functional scope...
+//MyCity = "Indore" can be changed but not in use
+//console.log(MyCity);
+
+console.table([MyId,MyPassword,MyEmail,MyCity,MyAccount])
---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all Js to new version
+
+alert (4+2)// we are using node js not the browser,
+
+//code readability should be High ;
+
+//Primitive data types...
+
+//number => 2 to power 53
+//bigint => for big value 
+// string => " "
+//boolean => Yes/No
+//null => Standalone value
+//undefined => Undefined 
+//symbol => uses for Unique value
+
+
+//object 
+
+console.log (typeof "age")
