768d93bc5006dfd2701df0099d477159fcac2650
jayjeet-singh <rajputjayjeetsingh20@gmail.com>
2025-06-10 12:40:33 +0000

    data conversion confusion

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,5 +1,26 @@
 let age = 23
 
-let Age = 32
+let Age = 11
+let Score = "22"
+let MyAccount = "37abc"
+let Paasword = undefined
+//console.log(typeof (Age) )
 
-console.log(Age)
+//console.log(typeof Score)
+//console.log(typeof MyAccount)
+console.log(typeof Paasword)
+let value = 1
+let booleanisLoggedIn = Boolean(value)
+console.log (booleanisLoggedIn)
+
+
+let valueInNumber = Number(MyAccount) 
+
+
+console.log (valueInNumber)
+console.log (typeof valueInNumber)
+
+//"22" => 22
+//"37abc" => NaN not a number
+//undefined => undefined
+// boolean => true/false
